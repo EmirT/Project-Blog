bc1998ba6bc195dd1f4608ef249edc626e69da5a
Emir Turan <1emir.turan@gmail.com>
2018-11-07 20:52:25 +0100

    added new images and working on the Book presentation on the below the page

--- index.php
+++ index.php
@@ -68,28 +68,27 @@
 
     <!-- Main Content -->
     <div class="container">
+   
       <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">
-                Man must explore, and this is exploration at its greatest
-              </h2>
-              <h3 class="post-subtitle">
-                Problems look mighty small from 150 miles up
-              </h3>
-            </a>
-            <p class="post-meta">Posted by
-              <a href="#">Start Bootstrap</a>
-              on September 24, 2018</p>
-          </div>
-        
-          <hr>
-          <!-- Pager -->
-          <div class="clearfix">
-            <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
-          </div>
-        </div>
+          <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="post-preview">
+               
+                <a href="post.html">
+                <img src="img/Book.png" class="col-sm-5 float-left" alt>
+                    <h2 class="post-title">
+                    Billet simple pour l'Alaska
+                    </h2>
+                    <h3 class="post-subtitle">
+                        Problems look mighty small from 150 miles up
+                    </h3>
+                    </a>
+                    <p class="post-meta">Posted by
+                    <a href="#">Start Bootstrap</a>
+                    on September 24, 2018</p>
+                </div>
+                
+                <hr>
+            </div>
       </div>
     </div>
 
